49930d221a1f20b448ca9f6b6f857a417ba54cfa
Bernardo Belini Vale <bernardobelinivale@gmail.com>
2025-11-01 08:25:23 -0300

    inicial

---
+++ public/app.js
@@ -0,0 +1,20 @@
+
+const postagens = [
+  {
+    id: "1",
+    autor: "Pedro Henrique",
+    data: "12 de setembro de 2025",
+    texto:"Fala pessoal, passando pra compartilhar uma situação chata que rolou com meu Fastback Abarth. Sempre gostei do carro, mas ultimamente venho percebendo um barulho estranho na suspensão, especialmente quando passo por buracos ou pisos irregulares. Parece um rangido que não tinha antes, e tô começando a ficar preocupado. Já conferi as buchas e parece que algumas estão meio gastas, mas ainda não levei no mecânico para um diagnóstico mais detalhado.",
+    membro: "10/06/2025",
+    imagem: "imagens/sherek.jpg"
+  },
+  {
+    id: "2",
+    autor: "Maria Clara",
+    data: "10 de setembro de 2025",
+    texto:"Fala pessoal, passando pra compartilhar uma situação chata que rolou com meu Fastback Abarth. Sempre gostei do carro, mas ultimamente venho percebendo um barulho estranho na suspensão, especialmente quando passo por buracos ou pisos irregulares. Parece um rangido que não tinha antes, e tô começando a ficar preocupado. Já conferi as buchas e parece que algumas estão meio gastas, mas ainda não levei no mecânico para um diagnóstico mais detalhado.",
+    membro: "19/04/2025",
+    imagem: "imagens/barney.jpeg"
+  }
+];
+
